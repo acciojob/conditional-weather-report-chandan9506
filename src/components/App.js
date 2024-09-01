4d6567f46ebad026438adeca9bfc146f78d65031
Chandan <chandan.ckmodi@gmail.com>
2024-09-01 11:55:06 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from 'react';
+import WeatherDisplay from './WeatherDisplay';
 
-const App = () => {
+function App() {
+  const [weather, setWeather] = useState({ temperature: 0, conditions: '' });
+
+  useEffect(() => {
+    const weatherInput = { temperature: 25, conditions: 'Sunny' };
+    setWeather(weatherInput);
+  }, []);
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <h1>Weather App</h1>
+      <WeatherDisplay weather={weather} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
